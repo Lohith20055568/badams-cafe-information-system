e252910f334b64bca7924d77bdb137269a0883bd
20055568 <lohithbadam10@gmail.com>
2026-03-18 14:47:33 +0000

    Set up Express server with JSON and static file support

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
 
+const app = express();
+const PORT = 3000;
+const DATA_FILE = path.join(__dirname, "data.json");
+
+app.use(express.json());
+app.use(express.static("client"));
