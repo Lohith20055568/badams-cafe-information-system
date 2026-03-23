9b2ee8f79cf6f27e3e3d70c2d871cfad9db91a26
20055568 <lohithbadam10@gmail.com>
2026-03-23 13:42:39 +0000

    Initialize Express server with required modules

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+const DATA_FILE = path.join(__dirname, "data.json");
 
